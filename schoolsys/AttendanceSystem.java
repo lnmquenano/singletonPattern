4ced6a36eb9ea5f5206a882f2a8ff8e10e9e084a
Lorenzo Nheo Queñano <enzoquenano993@gmail.com>
2025-10-08 03:30:18 +0800

    added schoolsys

---
+++ schoolsys/AttendanceSystem.java
@@ -0,0 +1,7 @@
+package schoolsys;
+
+public class AttendanceSystem {
+    void markAttendance() {
+        System.out.println("\nAttendance has been marked for today.");
+    }    
+}
---
+++ schoolsys/LibrarySystemAdapter.java
@@ -0,0 +1,14 @@
+package schoolsys;
+
+public class LibrarySystemAdapter implements SchoolManagementApp {
+    private LibrarySystem librarySystem;
+
+    public LibrarySystemAdapter(LibrarySystem librarySystem) {
+        this.librarySystem = librarySystem;
+    }
+
+    @Override
+    public void integrateSystem() {
+        librarySystem.manageBooks();
+    }
+}
