06ce90abc78c234b9994548099ecf18117fd3886
Lorenzo Nheo Queñano <enzoquenano993@gmail.com>
2025-10-07 20:34:45 +0800

    added main class

---
+++ schoolsys/ManageSystem.java
@@ -0,0 +1,11 @@
+package schoolsys;
+
+import java.util.Scanner;
+
+public class ManageSystem {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner (System.in);
+
+        
+    }
+}
